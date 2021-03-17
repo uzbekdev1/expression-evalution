22d5ac26c335e2f6dcc7d1c36be73bd0992a2a8a
uzbekdev1 <elyor@outlook>
2021-03-17 16:24:42 +0500

    basic math code evaluator

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.Write("Enter your script:");
+
+            var script = Console.ReadLine();
+
+            try
+            {
+                var result = await CSharpScript.EvaluateAsync(script);
+
+                Console.WriteLine($"Result={result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
